9e686bbd56cea5df0029cea990b9a3de6001b469
Alexey Deryugin <velovec@gmail.com>
2021-08-28 02:49:43 +0400

    Less bench

--- src/main.cc
+++ src/main.cc
@@ -19,14 +19,14 @@
 #define BUFF_SIZE 1024
 
 int main(int argc, const char *argv[]) {
-  for (int i = 0; i < 100000; i++ ) {
+  for (int i = 0; i < 5000; i++ ) {
     std::shared_ptr<ecdsa::Key> pKey = std::make_shared<ecdsa::Key>();
     auto pubKey = pKey->CreatePubKey();
     unsigned char hash160[20];
     auto addr = btc::Address::FromPublicKey(pubKey.get_pub_key_data(), 0, hash160);
 
     auto addrString = addr.ToString();
-    auto pKeyWif = btc::wif::PrivateKeyToWif(pKey->get_priv_key_data());
+    // auto pKeyWif = btc::wif::PrivateKeyToWif(pKey->get_priv_key_data());
 
     // std::cout << "Address: " << addr.ToString() << std::endl;
     // std::cout << "Private key: " << base58::EncodeBase58(pKey->get_priv_key_data()) << std::endl;
