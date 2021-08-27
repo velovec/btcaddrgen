2778891ecafa0e4d4c90db755fc9758011e80975
Alexey Deryugin <velovec@gmail.com>
2021-08-28 02:57:14 +0400

    Some rework

--- src/main.cc
+++ src/main.cc
@@ -18,14 +18,14 @@
 
 #define BUFF_SIZE 1024
 
-struct Wallet {
-  std::shared_ptr<ecdsa::Key> pKey;
-  btc::Address addr;
-};
+namespace btc {
+  struct Wallet {
+    std::shared_ptr<ecdsa::Key> pKey;
+    btc::Address addr;
+  };
 
-int main(int argc, const char *argv[]) {
-  for (int i = 0; i < 5000; i++ ) {
-    Wallet wallet = new Wallet();
+  public Wallet generate() {
+    Wallet wallet;
 
     wallet.pKey = std::make_shared<ecdsa::Key>();
 
@@ -33,6 +33,14 @@ int main(int argc, const char *argv[]) {
     unsigned char hash160[20];
 
     wallet.addr = btc::Address::FromPublicKey(pubKey.get_pub_key_data(), 0, hash160);
+
+    return wallet;
+  }
+}
+
+int main(int argc, const char *argv[]) {
+  for (int i = 0; i < 5000; i++ ) {
+    btc::Wallet wallet = btc::generate()
 
     auto addrString = wallet.addr.ToString();
     // auto pKeyWif = btc::wif::PrivateKeyToWif(pKey->get_priv_key_data());
