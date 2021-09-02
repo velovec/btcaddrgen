8fd175bcb20ecfdaa8d6fa82a18275ab3d416cf6
Alexey Deryugin <velovec@gmail.com>
2021-09-02 23:44:29 +0400

    Add RabbitMQ library

--- src/main.cc
+++ src/main.cc
@@ -13,6 +13,9 @@
 #include <ecdsa/base58.h>
 #include <ecdsa/key.h>
 
+#include <rabbitmq-c/amqp.h>
+#include <rabbitmq-c/tcp_socket.h>
+
 #include "btcwallet.h"
 #include "btcaddr.h"
 #include "btcwif.h"
@@ -20,15 +23,58 @@
 #define BUFF_SIZE 1024
 
 int main(int argc, const char *argv[]) {
+
+  char const *hostname = "localhost";
+  int port = 5620, status;
+  char const *exchange = "";
+  char const *routingkey = "";
+
+  amqp_socket_t *socket = NULL;
+  amqp_connection_state_t conn;
+
+  if (argc < 6) {
+    fprintf(
+        stderr,
+        "Usage: amqp_sendstring host port exchange routingkey messagebody\n");
+    return 1;
+  }
+
+  conn = amqp_new_connection();
+
+  socket = amqp_tcp_socket_new(conn);
+  if (!socket) {
+    die("creating TCP socket");
+  }
+
+  status = amqp_socket_open(socket, hostname, port);
+  if (status) {
+    die("opening TCP socket");
+  }
+
+  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
+  amqp_channel_open(conn, 1);
+  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+
+
+
+
   for (int i = 0; i < 6000; i++ ) {
     btc::Wallet wallet = btc::Wallet::Generate();
 
-    auto addrString = wallet.GetAddress().ToString();
-    // auto pKeyWif = btc::wif::PrivateKeyToWif(pKey->get_priv_key_data());
+    std::string addrString = wallet.GetAddress().ToString();
 
-    // std::cout << "Address: " << addr.ToString() << std::endl;
-    // std::cout << "Private key: " << base58::EncodeBase58(pKey->get_priv_key_data()) << std::endl;
+    {
+      amqp_basic_properties_t props;
+      props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
+      props.content_type = amqp_cstring_bytes("text/plain");
+      props.delivery_mode = 2; /* persistent delivery mode */
+      die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange), amqp_cstring_bytes(routingkey), 0, 0, &props, amqp_cstring_bytes(addrString)), "Publishing");
+    }
   }
+
+  die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+  die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+  die_on_error(amqp_destroy_connection(conn), "Ending connection");
 
   return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define __UTILS_H__
 
 #include <vector>
+#include <rabbitmq-c/amqp.h>
 #include <stdint.h>
 
 namespace utils {
@@ -9,6 +10,15 @@ namespace utils {
 std::vector<uint8_t> ripemd160(const uint8_t *data, int len);
 
 std::vector<uint8_t> sha256(const uint8_t *data, int len);
+
+void die(const char *fmt, ...);
+extern void die_on_error(int x, char const *context);
+extern void die_on_amqp_error(amqp_rpc_reply_t x, char const *context);
+
+extern void amqp_dump(void const *buffer, size_t len);
+
+extern uint64_t now_microseconds(void);
+extern void microsleep(int usec);
 
 }  // namespace utils
 
