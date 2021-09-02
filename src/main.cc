009b508cc080dd21a4c16e5ec3c946c142153484
Alexey Deryugin <velovec@gmail.com>
2021-09-02 23:49:03 +0400

    Add RabbitMQ library

--- src/main.cc
+++ src/main.cc
@@ -33,28 +33,21 @@ int main(int argc, const char *argv[]) {
   amqp_socket_t *socket = NULL;
   amqp_connection_state_t conn;
 
-  if (argc < 6) {
-    fprintf(
-        stderr,
-        "Usage: amqp_sendstring host port exchange routingkey messagebody\n");
-    return 1;
-  }
-
   conn = amqp_new_connection();
 
   socket = amqp_tcp_socket_new(conn);
   if (!socket) {
-    die("creating TCP socket");
+    utils::die("creating TCP socket");
   }
 
   status = amqp_socket_open(socket, hostname, port);
   if (status) {
-    die("opening TCP socket");
+    utils::die("opening TCP socket");
   }
 
-  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
+  utils::die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
   amqp_channel_open(conn, 1);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+  utils::die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
 
 
 
@@ -69,13 +62,13 @@ int main(int argc, const char *argv[]) {
       props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
       props.content_type = amqp_cstring_bytes("text/plain");
       props.delivery_mode = 2; /* persistent delivery mode */
-      die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange), amqp_cstring_bytes(routingkey), 0, 0, &props, amqp_cstring_bytes(addrString)), "Publishing");
+      utils::die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange), amqp_cstring_bytes(routingkey), 0, 0, &props, amqp_cstring_bytes(addrString)), "Publishing");
     }
   }
 
-  die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
-  die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
-  die_on_error(amqp_destroy_connection(conn), "Ending connection");
+  utils::die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+  utils::die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+  utils::die_on_error(amqp_destroy_connection(conn), "Ending connection");
 
   return 0;
 }
