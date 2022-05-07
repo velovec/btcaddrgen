9b9e818d173b18046032b792691779af9826eb1f
Alexey Deryugin <velovec@gmail.com>
2022-05-07 21:48:17 +0400

    Enable BLOCK_END message each 1000 blocks

--- src/main.cc
+++ src/main.cc
@@ -26,6 +26,7 @@ struct bloom bloom2;
 struct bloom bloom3;
 
 bool running = true;
+int block_count = 0;
 
 void generate_random(short flag, void (*callback)(const std::vector<uint8_t>&, short, bool)) {
   std::vector<uint8_t> rnd;
@@ -101,10 +102,19 @@ void on_generate(const std::vector<uint8_t>& pKeyData, short flag, bool last) {
     }
   }
 
-//  if (last) {
-//    const auto p1 = std::chrono::system_clock::now();
-//    std::cout << "<!--XSUPERVISOR:BEGIN-->BLOCK_END:" << std::chrono::duration_cast<std::chrono::seconds>(p1.time_since_epoch()).count() << "<!--XSUPERVISOR:END-->" << std::endl;
-//  }
+  if (last) {
+    block_count++;
+
+    if (block_count == 1000) {
+      const auto p1 = std::chrono::system_clock::now();
+      std::cout << "<!--XSUPERVISOR:BEGIN-->BLOCK_END:";
+      std::cout << block_count;
+      std::cout << std::chrono::duration_cast<std::chrono::seconds>(p1.time_since_epoch()).count();
+      std::cout << "<!--XSUPERVISOR:END-->" << std::endl;
+
+      block_count = 0;
+    }
+  }
 }
 
 void die(const char *fmt, ...) {
